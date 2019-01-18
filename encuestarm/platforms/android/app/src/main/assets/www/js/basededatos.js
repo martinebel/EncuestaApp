e35c6cb345d2b0cd47dfcdf8beb6e956ee6a143f
martinebel <36831742+martinebel@users.noreply.github.com>
2019-01-17 21:10:31 -0300

    test build

--- encuestarm/platforms/android/app/src/main/assets/www/js/basededatos.js
+++ encuestarm/platforms/android/app/src/main/assets/www/js/basededatos.js
@@ -1,9 +1,11 @@
 var db = null;
+$("#estado").append("<p>iniciando</p>");
 document.addEventListener("deviceready", onDeviceReady, false);
 
 //iniciar base de datos, crear si no existe. consultar por una tabla, si no
 //devuelve filas, tengo que conectarme al servidor y leer los datos.
 function onDeviceReady() {
+  $("#estado").append("<p>abriendo bd</p>");
   db = window.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
 //crear las tablas si no existen
     db.transaction(function (tx) {
@@ -14,7 +16,7 @@ function onDeviceReady() {
     tx.executeSql('CREATE TABLE IF NOT EXISTS  tipos (id INTEGER,clase)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS  usuarios (idUsuario INTEGER,nombre,password,tipo)');
 }, function (error) {
-    alert('transaction error1: ' + error.message);
+    $("#estado").append('<p>transaction error1: ' + error.message+'</p>');
 }, function () {
     //hacer una consulta a usuarios. si está vacia tengo que conectarme al servidor
     //y descargar los datos
@@ -24,15 +26,15 @@ function onDeviceReady() {
         window.location.href="login.html";
         },
         function (tx, error) {
-            alert('SELECT error: ' + error.message);
+          $("#estado").append('<p>SELECT error: ' + error.message+'</p>');
         });
     }, function (error) {
-        alert('transaction error2: ' + error.message);
+        $("#estado").append('<p>transaction error2: ' + error.message+'</p>');
     });
 });
 
 }, function (error) {
-    alert('Open database ERROR1: ' + JSON.stringify(error));
+  $("#estado").append('<p>Open database ERROR1: ' + JSON.stringify(error)+'</p>');
 });
 }
 
@@ -51,12 +53,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into elecciones (id ,descripcion) values ("+datae[i].id+",'"+datae[i].descripcion+"')");
   }, function (error) {
-      alert('transaction error elecciones: ' + error.message);
+      $("#estado").append('<p>transaction error elecciones: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error elecciones");
 });
 
 //copiar tabla encuestas
@@ -72,12 +74,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into encuestas (id ,titulo,fecha_inicio,fecha_cierre,fecha_creacion) values ("+datae[i].id+",'"+datae[i].titulo+"','"+datae[i].fecha_inicio+"','"+datae[i].fecha_cierre+"','"+datae[i].fecha_creacion+"')");
   }, function (error) {
-      alert('transaction error encuestas: ' + error.message);
+      $("#estado").append('<p>transaction error encuestas: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error encuestas");
 });
 
 //copiar tabla opciones
@@ -93,12 +95,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into opciones (id,eleccion_id,tipo_id,pregunta_id,estado) values ("+datae[i].id+",'"+datae[i].eleccion_id+"','"+datae[i].tipo_id+"','"+datae[i].pregunta_id+"',null)");
   }, function (error) {
-      alert('transaction error opciones: ' + error.message);
+      $("#estado").append('<p>transaction error opciones: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error opciones");
 });
 
 //copiar tabla preguntas
@@ -114,12 +116,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into preguntas (id ,descripcion,encuesta_id) values ("+datae[i].id+",'"+datae[i].descripcion+"','"+datae[i].encuesta_id+"')");
   }, function (error) {
-      alert('transaction error preguntas: ' + error.message);
+      $("#estado").append('<p>transaction error preguntas: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error preguntas");
 });
 
 //copiar tabla tipos
@@ -135,12 +137,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into tipos (id ,clase) values ("+datae[i].id+",'"+datae[i].clase+"')");
   }, function (error) {
-      alert('transaction error tipos: ' + error.message);
+      $("#estado").append('<p>transaction error tipos: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error tipos");
 });
 
 //copiar tabla usuarios
@@ -156,12 +158,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into usuarios (id ,nombre,password,tipo) values ("+datae[i].id+",'"+datae[i].nombre+"','"+datae[i].password+"','"+datae[i].tipo+"')");
   }, function (error) {
-      alert('transaction error usuarios: ' + error.message);
+      alert('<p>transaction error usuarios: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error usuarios");
 });
 
 }
--- encuestarm/www/js/basededatos.js
+++ encuestarm/www/js/basededatos.js
@@ -1,11 +1,14 @@
 var db = null;
+$("#estado").append("<p>iniciando</p>");
 document.addEventListener("deviceready", onDeviceReady, false);
 
 //iniciar base de datos, crear si no existe. consultar por una tabla, si no
 //devuelve filas, tengo que conectarme al servidor y leer los datos.
 function onDeviceReady() {
+  $("#estado").append("<p>abriendo bd</p>");
   db = window.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
 //crear las tablas si no existen
+$("#estado").append("<p>bd abierta</p>");
     db.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS  elecciones (id INTEGER PRIMARY KEY,descripcion)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS  encuestas (id INTEGER,titulo,fecha_inicio,fecha_cierre,fecha_creacion)');
@@ -13,8 +16,9 @@ function onDeviceReady() {
     tx.executeSql('CREATE TABLE IF NOT EXISTS  preguntas (id INTEGER,descripcion,encuesta_id INTEGER)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS  tipos (id INTEGER,clase)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS  usuarios (idUsuario INTEGER,nombre,password,tipo)');
+    $("#estado").append("<p>create table finalizado</p>");
 }, function (error) {
-    alert('transaction error1: ' + error.message);
+    $("#estado").append('<p>transaction error1: ' + error.message+'</p>');
 }, function () {
     //hacer una consulta a usuarios. si está vacia tengo que conectarme al servidor
     //y descargar los datos
@@ -24,15 +28,15 @@ function onDeviceReady() {
         window.location.href="login.html";
         },
         function (tx, error) {
-            alert('SELECT error: ' + error.message);
+          $("#estado").append('<p>SELECT error: ' + error.message+'</p>');
         });
     }, function (error) {
-        alert('transaction error2: ' + error.message);
+        $("#estado").append('<p>transaction error2: ' + error.message+'</p>');
     });
 });
 
 }, function (error) {
-    alert('Open database ERROR1: ' + JSON.stringify(error));
+  $("#estado").append('<p>Open database ERROR1: ' + JSON.stringify(error)+'</p>');
 });
 }
 
@@ -51,12 +55,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into elecciones (id ,descripcion) values ("+datae[i].id+",'"+datae[i].descripcion+"')");
   }, function (error) {
-      alert('transaction error elecciones: ' + error.message);
+      $("#estado").append('<p>transaction error elecciones: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error elecciones");
 });
 
 //copiar tabla encuestas
@@ -72,12 +76,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into encuestas (id ,titulo,fecha_inicio,fecha_cierre,fecha_creacion) values ("+datae[i].id+",'"+datae[i].titulo+"','"+datae[i].fecha_inicio+"','"+datae[i].fecha_cierre+"','"+datae[i].fecha_creacion+"')");
   }, function (error) {
-      alert('transaction error encuestas: ' + error.message);
+      $("#estado").append('<p>transaction error encuestas: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error encuestas");
 });
 
 //copiar tabla opciones
@@ -93,12 +97,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into opciones (id,eleccion_id,tipo_id,pregunta_id,estado) values ("+datae[i].id+",'"+datae[i].eleccion_id+"','"+datae[i].tipo_id+"','"+datae[i].pregunta_id+"',null)");
   }, function (error) {
-      alert('transaction error opciones: ' + error.message);
+      $("#estado").append('<p>transaction error opciones: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error opciones");
 });
 
 //copiar tabla preguntas
@@ -114,12 +118,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into preguntas (id ,descripcion,encuesta_id) values ("+datae[i].id+",'"+datae[i].descripcion+"','"+datae[i].encuesta_id+"')");
   }, function (error) {
-      alert('transaction error preguntas: ' + error.message);
+      $("#estado").append('<p>transaction error preguntas: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error preguntas");
 });
 
 //copiar tabla tipos
@@ -135,12 +139,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into tipos (id ,clase) values ("+datae[i].id+",'"+datae[i].clase+"')");
   }, function (error) {
-      alert('transaction error tipos: ' + error.message);
+      $("#estado").append('<p>transaction error tipos: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error tipos");
 });
 
 //copiar tabla usuarios
@@ -156,12 +160,12 @@ $.ajax({
   db.transaction(function (tx) {
       tx.executeSql("insert into usuarios (id ,nombre,password,tipo) values ("+datae[i].id+",'"+datae[i].nombre+"','"+datae[i].password+"','"+datae[i].tipo+"')");
   }, function (error) {
-      alert('transaction error usuarios: ' + error.message);
+      alert('<p>transaction error usuarios: ' + error.message+'</p>');
   });
 }
 })
 .fail(function(jqXHR, textStatus, errorThrown){
-alert("error");
+$("#estado").append("<p>error usuarios");
 });
 
 }
