99d7e09668bfbd5fff731a9a019e3ad6ea0a7cc7
martinebel <36831742+martinebel@users.noreply.github.com>
2019-01-18 11:44:00 -0300

    bd data download routines

---
+++ encuestarm/www/js/init.js
@@ -0,0 +1,228 @@
+mensaje("Conectando al servidor");
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+elecciones();
+encuestas();
+opciones();
+preguntas();
+tipos();
+usuarios();
+window.location.href="login.html";
+}
+
+function elecciones()
+{
+var db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
+
+  $.ajax({
+              url: "http://192.168.2.101/EncuestaApp/bdfetch.php?tabla=elecciones",
+              dataType: "json",
+              async: false,
+              success: function(res) {
+                mensaje("Copiando datos 1/6");
+                db.transaction(function(tx) {
+                  $.each(res, function(i, item) {
+                    tx.executeSql("INSERT INTO elecciones values (?,?)", [JSON.stringify(item.id),JSON.stringify(item.descripcion)]);
+
+                  });
+                }, function(e) {
+                  mensaje('Transaction error: ' + e.message);
+                  alert('Transaction error: ' + e.message);
+                }, function() {
+
+                });
+              },
+              error: function(e) {
+                  mensaje('ajax error: ' + JSON.stringify(e));
+                  alert('ajax error: ' + JSON.stringify(e));
+              }
+          });
+
+
+}, function (error) {
+mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+function encuestas()
+{
+var db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
+
+  $.ajax({
+              url: "http://192.168.2.101/EncuestaApp/bdfetch.php?tabla=encuestas",
+              dataType: "json",
+              async: false,
+              success: function(res) {
+                mensaje("Copiando datos 2/6");
+                db.transaction(function(tx) {
+                  $.each(res, function(i, item) {
+                    tx.executeSql("INSERT INTO encuestas values (?,?,?,?,?)", [JSON.stringify(item.id),JSON.stringify(item.titulo),JSON.stringify(item.fecha_inicio),JSON.stringify(item.fecha_cierre),JSON.stringify(item.fecha_creacion)]);
+
+                  });
+                }, function(e) {
+                  mensaje('Transaction error: ' + e.message);
+                  alert('Transaction error: ' + e.message);
+                }, function() {
+
+                });
+              },
+              error: function(e) {
+                  mensaje('ajax error: ' + JSON.stringify(e));
+                  alert('ajax error: ' + JSON.stringify(e));
+              }
+          });
+
+
+}, function (error) {
+mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+function opciones()
+{
+var db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
+
+  $.ajax({
+              url: "http://192.168.2.101/EncuestaApp/bdfetch.php?tabla=opciones",
+              dataType: "json",
+              async: false,
+              success: function(res) {
+                mensaje("Copiando datos 3/6");
+                db.transaction(function(tx) {
+                  $.each(res, function(i, item) {
+                    tx.executeSql("INSERT INTO opciones values (?,?,?,?,?)", [JSON.stringify(item.id),JSON.stringify(item.eleccion_id),JSON.stringify(item.tipo_id),JSON.stringify(item.pregunta_id),JSON.stringify(item.estado)]);
+
+                  });
+                }, function(e) {
+                  mensaje('Transaction error: ' + e.message);
+                  alert('Transaction error: ' + e.message);
+                }, function() {
+
+                });
+              },
+              error: function(e) {
+                  mensaje('ajax error: ' + JSON.stringify(e));
+                  alert('ajax error: ' + JSON.stringify(e));
+              }
+          });
+
+
+}, function (error) {
+mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+function preguntas()
+{
+var db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
+
+  $.ajax({
+              url: "http://192.168.2.101/EncuestaApp/bdfetch.php?tabla=preguntas",
+              dataType: "json",
+              async: false,
+              success: function(res) {
+                mensaje("Copiando datos 4/6");
+                db.transaction(function(tx) {
+                  $.each(res, function(i, item) {
+                    tx.executeSql("INSERT INTO preguntas values (?,?,?)", [JSON.stringify(item.id),JSON.stringify(item.descripcion),JSON.stringify(item.encuesta_id)]);
+
+                  });
+                }, function(e) {
+                  mensaje('Transaction error: ' + e.message);
+                  alert('Transaction error: ' + e.message);
+                }, function() {
+
+                });
+              },
+              error: function(e) {
+                  mensaje('ajax error: ' + JSON.stringify(e));
+                  alert('ajax error: ' + JSON.stringify(e));
+              }
+          });
+
+
+}, function (error) {
+mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+function tipos()
+{
+var db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
+
+  $.ajax({
+              url: "http://192.168.2.101/EncuestaApp/bdfetch.php?tabla=tipos",
+              dataType: "json",
+              async: false,
+              success: function(res) {
+                mensaje("Copiando datos 5/6");
+                db.transaction(function(tx) {
+                  $.each(res, function(i, item) {
+                    tx.executeSql("INSERT INTO tipos values (?,?)", [JSON.stringify(item.id),JSON.stringify(item.clase)]);
+
+                  });
+                }, function(e) {
+                  mensaje('Transaction error: ' + e.message);
+                  alert('Transaction error: ' + e.message);
+                }, function() {
+
+                });
+              },
+              error: function(e) {
+                  mensaje('ajax error: ' + JSON.stringify(e));
+                  alert('ajax error: ' + JSON.stringify(e));
+              }
+          });
+
+
+}, function (error) {
+mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+function usuarios()
+{
+var db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
+
+  $.ajax({
+              url: "http://192.168.2.101/EncuestaApp/bdfetch.php?tabla=usuarios",
+              dataType: "json",
+              async: false,
+              success: function(res) {
+                mensaje("Copiando datos 6/6");
+                db.transaction(function(tx) {
+                  $.each(res, function(i, item) {
+                    tx.executeSql("INSERT INTO usuarios values (?,?,?,?)", [JSON.stringify(item.idUsuario),JSON.stringify(item.nombre),JSON.stringify(item.password),JSON.stringify(item.tipo)]);
+
+                  });
+                }, function(e) {
+                  mensaje('Transaction error: ' + e.message);
+                  alert('Transaction error: ' + e.message);
+                }, function() {
+
+                });
+              },
+              error: function(e) {
+                  mensaje('ajax error: ' + JSON.stringify(e));
+                  alert('ajax error: ' + JSON.stringify(e));
+              }
+          });
+
+
+}, function (error) {
+mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+
+
+function mensaje(msg)
+{
+  $("#estado p").fadeOut(300, function() { $("#estado p").remove(); });
+  $('#estado')
+    .append($('<p>'+msg+'</p>')
+        .hide()
+        .fadeIn(300)
+    );
+}
