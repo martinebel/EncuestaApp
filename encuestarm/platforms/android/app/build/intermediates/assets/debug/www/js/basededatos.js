175e06001a2a2e6c065757bbb5b457c1da7b24f7
martinebel <36831742+martinebel@users.noreply.github.com>
2019-01-17 21:56:06 -0300

    test build

--- encuestarm/platforms/android/app/build/intermediates/assets/debug/www/js/basededatos.js
+++ encuestarm/platforms/android/app/build/intermediates/assets/debug/www/js/basededatos.js
@@ -1,178 +1,37 @@
+
 var db = null;
 $("#estado").append("<p>iniciando</p>");
-document.addEventListener("deviceready", onDeviceReady, false);
+
 
 //iniciar base de datos, crear si no existe. consultar por una tabla, si no
 //devuelve filas, tengo que conectarme al servidor y leer los datos.
-function onDeviceReady() {
-  $("#estado").append("<p>abriendo bd</p>");
-  db = window.sqlitePlugin.openDatabase({ name: 'encuestarm.db', location: 'default' }, function (db) {
-//crear las tablas si no existen
-$("#estado").append("<p>bd abierta</p>");
-    db.transaction(function (tx) {
+document.addEventListener('deviceready', function() {
+  db = window.sqlitePlugin.openDatabase({
+    name: 'my.db',
+    location: 'default',
+  });
+
+  db.transaction(function(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DemoTable (name, score)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS  elecciones (id INTEGER PRIMARY KEY,descripcion)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS  encuestas (id INTEGER,titulo,fecha_inicio,fecha_cierre,fecha_creacion)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS  opciones (id INTEGER  PRIMARY KEY AUTOINCREMENT,eleccion_id,tipo_id,pregunta_id,estado)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS  preguntas (id INTEGER,descripcion,encuesta_id INTEGER)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS  tipos (id INTEGER,clase)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS  usuarios (idUsuario INTEGER,nombre,password,tipo)');
-    tx.executeSql("insert into  usuarios (idUsuario INTEGER,nombre,password,tipo) values (0,'martin','123','usuario')");
-    $("#estado").append("<p>create table finalizado</p>");
-}, function (error) {
-    $("#estado").append('<p>transaction error1: ' + error.message+'</p>');
-}, function () {
-    //hacer una consulta a usuarios. si está vacia tengo que conectarme al servidor
-    //y descargar los datos
-
-});
-
-db.transaction(function (tx) {
-
-  tx.executeSql("SELECT * from usuarios", function (tx, resultSet) {
-    $("#estado").append('<p>SELECT results: ' +resultSet.rows.length+'</p>');
-    if(resultSet.rows.length==0){populateDB();}
-    window.location.href="login.html";
-    },
-    function (tx, error) {
-        $("#estado").append('<p>SELECT error: ' + error.message+'</p>');
-    });
-}, function (error) {
-  $("#estado").append('<p>transaction error: ' + error.message+'</p>');
-}, function () {
-    $("#estado").append('<p>transaction ok'+'</p>');
-});
-
-}, function (error) {
-  $("#estado").append('<p>Open database ERROR1: ' + JSON.stringify(error)+'</p>');
-});
-}
-
-function populateDB()
-{
-//copiar tabla elecciones
-$.ajax({
- type: "POST",
- crossDomain: true,
- url: urlAPI+"bdfetch.php?tabla=elecciones",
- processData: false,
- contentType: "application/json"
-})
-.success(function(datae, textStatus, jqXHR){
-  for(var i=0;i<datae.length;i++){
-  db.transaction(function (tx) {
-      tx.executeSql("insert into elecciones (id ,descripcion) values ("+datae[i].id+",'"+datae[i].descripcion+"')");
-  }, function (error) {
-      $("#estado").append('<p>transaction error elecciones: ' + error.message+'</p>');
+    tx.executeSql('CREATE TABLE  IF NOT EXISTS encuestas (id INTEGER,titulo,fecha_inicio,fecha_cierre,fecha_creacion)');
+    tx.executeSql('CREATE TABLE  IF NOT EXISTS opciones (id INTEGER  PRIMARY KEY AUTOINCREMENT,eleccion_id,tipo_id,pregunta_id,estado)');
+    tx.executeSql('CREATE TABLE  IF NOT EXISTS preguntas (id INTEGER,descripcion,encuesta_id INTEGER)');
+    tx.executeSql('CREATE TABLE  IF NOT EXISTS tipos (id INTEGER,clase)');
+    tx.executeSql('CREATE TABLE  IF NOT EXISTS usuarios (idUsuario INTEGER,nombre,password,tipo)');
+    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Alice', 101]);
+    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Betty', 202]);
+  }, function(error) {
+      $("#estado").append('<p>Transaction ERROR: ' + error.message+'</p>');
+  }, function() {
+      $("#estado").append('<p>Populated database OK'+'</p>');
   });
-}
-})
-.fail(function(jqXHR, textStatus, errorThrown){
-$("#estado").append("<p>error elecciones");
-});
 
-//copiar tabla encuestas
-$.ajax({
- type: "POST",
- crossDomain: true,
- url: urlAPI+"bdfetch.php?tabla=encuestas",
- processData: false,
- contentType: "application/json"
-})
-.success(function(datae, textStatus, jqXHR){
-  for(var i=0;i<datae.length;i++){
-  db.transaction(function (tx) {
-      tx.executeSql("insert into encuestas (id ,titulo,fecha_inicio,fecha_cierre,fecha_creacion) values ("+datae[i].id+",'"+datae[i].titulo+"','"+datae[i].fecha_inicio+"','"+datae[i].fecha_cierre+"','"+datae[i].fecha_creacion+"')");
-  }, function (error) {
-      $("#estado").append('<p>transaction error encuestas: ' + error.message+'</p>');
-  });
-}
-})
-.fail(function(jqXHR, textStatus, errorThrown){
-$("#estado").append("<p>error encuestas");
+  db.transaction(function(tx) {
+   tx.executeSql('SELECT count(*) AS mycount FROM usuarios', [], function(tx, rs) {
+      $("#estado").append('<p>Record count (expected to be 2): ' + rs.rows.item(0).mycount+'</p>');
+   }, function(tx, error) {
+       $("#estado").append('<p>SELECT error: ' + error.message+'</p>');
+   });
+ });
 });
-
-//copiar tabla opciones
-$.ajax({
- type: "POST",
- crossDomain: true,
- url: urlAPI+"bdfetch.php?tabla=opciones",
- processData: false,
- contentType: "application/json"
-})
-.success(function(datae, textStatus, jqXHR){
-  for(var i=0;i<datae.length;i++){
-  db.transaction(function (tx) {
-      tx.executeSql("insert into opciones (id,eleccion_id,tipo_id,pregunta_id,estado) values ("+datae[i].id+",'"+datae[i].eleccion_id+"','"+datae[i].tipo_id+"','"+datae[i].pregunta_id+"',null)");
-  }, function (error) {
-      $("#estado").append('<p>transaction error opciones: ' + error.message+'</p>');
-  });
-}
-})
-.fail(function(jqXHR, textStatus, errorThrown){
-$("#estado").append("<p>error opciones");
-});
-
-//copiar tabla preguntas
-$.ajax({
- type: "POST",
- crossDomain: true,
- url: urlAPI+"bdfetch.php?tabla=preguntas",
- processData: false,
- contentType: "application/json"
-})
-.success(function(datae, textStatus, jqXHR){
-  for(var i=0;i<datae.length;i++){
-  db.transaction(function (tx) {
-      tx.executeSql("insert into preguntas (id ,descripcion,encuesta_id) values ("+datae[i].id+",'"+datae[i].descripcion+"','"+datae[i].encuesta_id+"')");
-  }, function (error) {
-      $("#estado").append('<p>transaction error preguntas: ' + error.message+'</p>');
-  });
-}
-})
-.fail(function(jqXHR, textStatus, errorThrown){
-$("#estado").append("<p>error preguntas");
-});
-
-//copiar tabla tipos
-$.ajax({
- type: "POST",
- crossDomain: true,
- url: urlAPI+"bdfetch.php?tabla=tipos",
- processData: false,
- contentType: "application/json"
-})
-.success(function(datae, textStatus, jqXHR){
-  for(var i=0;i<datae.length;i++){
-  db.transaction(function (tx) {
-      tx.executeSql("insert into tipos (id ,clase) values ("+datae[i].id+",'"+datae[i].clase+"')");
-  }, function (error) {
-      $("#estado").append('<p>transaction error tipos: ' + error.message+'</p>');
-  });
-}
-})
-.fail(function(jqXHR, textStatus, errorThrown){
-$("#estado").append("<p>error tipos");
-});
-
-//copiar tabla usuarios
-$.ajax({
- type: "POST",
- crossDomain: true,
- url: urlAPI+"bdfetch.php?tabla=usuarios",
- processData: false,
- contentType: "application/json"
-})
-.success(function(datae, textStatus, jqXHR){
-  for(var i=0;i<datae.length;i++){
-  db.transaction(function (tx) {
-      tx.executeSql("insert into usuarios (id ,nombre,password,tipo) values ("+datae[i].id+",'"+datae[i].nombre+"','"+datae[i].password+"','"+datae[i].tipo+"')");
-  }, function (error) {
-      alert('<p>transaction error usuarios: ' + error.message+'</p>');
-  });
-}
-})
-.fail(function(jqXHR, textStatus, errorThrown){
-$("#estado").append("<p>error usuarios");
-});
-
-}
